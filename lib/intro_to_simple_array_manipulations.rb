5d568bca8efdbe41bd40653c597966e06e8cd029
Bashir <bashiralhanshali@yahoo.com>
2020-04-22 20:08:54 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,14 @@
+def using_push(array, str)
+  array.push(str)
+end 
+
+def using_unshift(array, str)
+  array.unshift(str)
+end 
+
+def using_pop(array)
+  array.pop 
+end 
+
+def pop_with_args(array, index)
+  
